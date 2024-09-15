d0a9e6b0653a6ddea0579880765e2956da5996e6
Simon-LM <simon.lm.dev@bm.me>
2024-09-15 23:35:26 +0200

    Arborescent

---
+++ P11-ArgentBank/src/layouts/footer/Footer.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import "./Footer.scss";
+
+const Footer: React.FC = () => {
+	return (
+		<footer className="footer">
+			<p className="footer-text">Copyright 2020 Argent Bank</p>
+		</footer>
+	);
+};
+
+export default Footer;
--- P11-ArgentBank/src/main.tsx
+++ P11-ArgentBank/src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.tsx";
 import store from "./store/Store.tsx";
-import "./styles/index.css";
+import "./styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
