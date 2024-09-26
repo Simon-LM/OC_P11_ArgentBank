e9ca833011fa9279133a2255c8825da468be2855
Simon-LM <simon.lm.dev@bm.me>
2024-09-26 22:04:41 +0200

    Light & Dark Mode DONE

--- P11-ArgentBank/src/pages/home/Home.tsx
+++ P11-ArgentBank/src/pages/home/Home.tsx
@@ -6,7 +6,14 @@ import React from "react";
 const Home: React.FC = () => {
 	return (
 		<main>
-			<div className="hero">
+			<div
+				className="hero"
+				aria-label="A tree sprout in a verse filled with coins. 
+">
+				<div className="hero-image-description">
+					<span className="material-symbols-outlined">image</span> A tree sprout
+					in a verse filled with coins.
+				</div>
 				<section className="hero-content">
 					<h2 className="sr-only">Promoted Content</h2>
 					<p className="subtitle">No fees.</p>
@@ -18,11 +25,14 @@ const Home: React.FC = () => {
 			<section className="features">
 				<h2 className="sr-only">Features</h2>
 				<div className="feature-item">
-					<img
-						src="./src/assets/img/icon-chat.png"
-						alt="Chat Icon"
-						className="feature-icon"
-					/>
+					<div className="feature-item-icon">
+						<i className="feature-icon  feature-icon-1" aria-label="Chat icon">
+							<div className="icon-image-description">
+								<span className="material-symbols-outlined">image</span> Chat
+								icon
+							</div>
+						</i>
+					</div>
 					<h3 className="feature-item-title">You are our #1 priority</h3>
 					<p>
 						Need to talk to a representative? You can get in touch through our
@@ -30,11 +40,14 @@ const Home: React.FC = () => {
 					</p>
 				</div>
 				<div className="feature-item">
-					<img
-						src="./src/assets/img/icon-money.png"
-						alt="Money Icon"
-						className="feature-icon"
-					/>
+					<div className="feature-item-icon">
+						<i className="feature-icon feature-icon-2" aria-label="Money icon">
+							<div className="icon-image-description">
+								<span className="material-symbols-outlined">image</span> Money
+								icon
+							</div>
+						</i>
+					</div>
 					<h3 className="feature-item-title">
 						More savings means higher rates
 					</h3>
@@ -42,12 +55,17 @@ const Home: React.FC = () => {
 						The more you save with us, the higher your interest rate will be!
 					</p>
 				</div>
-				<div className="feature-item">
-					<img
-						src="./src/assets/img/icon-security.png"
-						alt="Security Icon"
-						className="feature-icon"
-					/>
+				<div className="feature-item ">
+					<div className="feature-item-icon">
+						<i
+							className="feature-icon  feature-icon-3"
+							aria-label="Securitiy icon">
+							<div className="icon-image-description">
+								<span className="material-symbols-outlined">image</span>{" "}
+								Securitiy icon
+							</div>
+						</i>
+					</div>
 					<h3 className="feature-item-title">Security you can trust</h3>
 					<p>
 						We use top of the line encryption to make sure your data and money
