71e6a529c115c4402ece7c1d3b4dee2fc81cd3d2
Simon-LM <simo.lm.dev@pm.me>
2025-05-05 23:18:15 +0200

    feat(ui): improve responsiveness and accessibility for all screen sizes

--- P11-ArgentBank/src/pages/signIn/signin.module.scss.d.ts
+++ P11-ArgentBank/src/pages/signIn/signin.module.scss.d.ts
@@ -1,8 +1,8 @@
 declare const classNames: {
-  readonly "signin-form": "signin-form";
   readonly "signin-form__input-group": "signin-form__input-group";
   readonly "signin-form__error": "signin-form__error";
   readonly "signin-page": "signin-page";
+  readonly "signin-form": "signin-form";
   readonly "signin-form__icon": "signin-form__icon";
   readonly "signin-form__remember": "signin-form__remember";
   readonly "signin-form__button": "signin-form__button";
